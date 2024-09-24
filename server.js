53be7ff85de3bb24df504d096b154367082375ba
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-09-24 13:24:57 +0900

    fix server

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! Welcome to Node.js');
+  const filePath = req.url === '/' ? 'index.html' : req.url;
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.svg':
+      contentType = 'image/svg+xml';
+      break;
+  }
+
+  fs.readFile(path.join(__dirname, filePath), (err, content) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('<h1>404 Not Found</h1>', 'utf-8');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const PORT = 13000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
